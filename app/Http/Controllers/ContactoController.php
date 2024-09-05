4efd148657b337bfce5ca4e91b9c2796df5c083b
Citlali Hermosillo <mariacitlalihermosillo2004@gmail.com>
2024-09-05 16:26:05 -0600

    cambio

---
+++ app/Http/Controllers/ContactoController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contacto;
+use Illuminate\Http\Request;
+
+
+class ContactoController extends Controller
+{
+    public function listado()
+    {
+        $mensajes = Contacto::all();
+        return view('/listado-contactos', compact('mensajes'));
+
+        //return view('/listado-contactos', ['mensajes' => $mensajes]);
+
+        //return view('/listado-contactos', ['mensajes' => Contacto::all()]);
+    }
+
+    public function formularioContacto()
+    {
+        return view('contacto');
+    }
+
+    public function guardarFormulario(Request $request)
+    {
+        //Recibir datos
+        //validar datos
+        //guardar datos
+        $contacto = new Contacto();
+        $contacto->nombre = $request->nombre;
+        $contacto->correo = $request->correo;
+        $contacto->mensaje = $request->correo;
+        $contacto->save();
+
+        return redirect('/contacto');
+    }
+}
